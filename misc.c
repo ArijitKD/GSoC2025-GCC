799c13505e403e9d402898dd7720251753bf0fe1
Arijit Kumar Das <arijitkdgit.official@gmail.com>
2025-06-23 23:34:57 +0530

    Updated open() and refactored code
    Signed-off-by: Arijit Kumar Das <arijitkdgit.official@gmail.com>

--- misc.c
+++ misc.c
@@ -45,7 +45,7 @@ enum FileSystemLimits {
   MAX_FOPEN = 5			// Maximum number of simultaneously open files 
 };
 
-enum FileOpenModes {
+enum SupportedFileOpenModes {
   MODE_R = O_RDONLY,
   MODE_W = (O_WRONLY | O_CREAT | O_TRUNC),
   MODE_A = (O_WRONLY | O_CREAT | O_APPEND),
@@ -62,84 +62,107 @@ enum FileIOErrors {
 
 // This is the actual file data structure with its metadata
 struct File {
-  ssize_t fid;			// File ID; maps to an index in vramdisk[]
+  int fid;			// File ID; maps to an index in vramdisk[]
   char fname[MAX_FNAME];	// Null-terminated string to store file name
   size_t fsize;			// Store file size in bytes
-  uint8_t data[MAX_FSIZE];	// Actual file data
+  char data[MAX_FSIZE];		// Actual file data
 };
 
 // This is a VRAM buffer simulating a disk to store all the files
 // Indices of this "VRAM disk" is the file ID in the file system
 // WARNING: This initialization is not standard C, but GCC supported
 static struct File vramdisk[MAX_FILES] = {[0 ... MAX_FILES-1] = {
-	.fid = -1,
-	.fname = "",
-	.fsize = 0,
-	.data = ""
+  .fid = -1,
+  .fname = "",
+  .fsize = 0,
+  .data = ""
 }};
 
 // This is the data structure that stores metadata about a currently open file
 struct OpenFile {
-  ssize_t fd;		// File descriptor; essentially the File ID
-  off_t foffset;	// Current read/write offset within the file (0 <= offset <= fsize)
+  int fd;		// File descriptor; essentially the File ID
+  off_t offset;		// Current read/write offset within the file (0 <= offset <= fsize)
   int mode;		// The mode in which the file was opened
 };
 
 // The file table for all open files. All file IDs default to -1, indicating an empty entry in this file table.
 static struct OpenFile open_files[MAX_FOPEN] = {[0 ... MAX_FOPEN-1] = {
-	.fd = -1,
-	.foffset = 0,
-	.mode = -1
+  .fd = -1,
+  .offset = 0,
+  .mode = -1
 }};
 
 // A variable to track the next open file index from open_files
-static ssize_t next_open_file_index = 0;
+static int next_open_file_index = 0;
 
 
 /**************************************** INTERNAL SUBROUTINES ****************************************/
 
-static ssize_t find_file(const char *fname) {
-/* Linearly searches for the file with filename == fname and returns its file ID, or ERR_FILE_NOT_FOUND
- * on failure
+static int find_file(const char *fname, int *fid_addr) {
+/* Searches for the file with name fname in the file system.
+ * Linearly searches for the file with filename == fname, stores its file ID at fid_addr and returns 0
+ * on success, or ERR_FILE_NOT_FOUND on failure.
  *
  * TODO: Optimize searching algorithm
  */
-  for (size_t i = 0; i < MAX_FILES; ++i) {
-    if (vramdisk[i].fid != -1 && !strcmp(vramdisk[i].fname, fname))
-      return vramdisk[i].fid;
+  for (int i = 0; i < MAX_FILES; ++i) {
+    if (vramdisk[i].fid != -1 && !strcmp(vramdisk[i].fname, fname)) {
+      *fid_addr = vramdisk[i].fid;
+      return 0;
+    }
   }
   return ERR_FILE_NOT_FOUND;
 }
 
-static ssize_t create_file(const char *fname) {
-/* Linearly scans through all entries in vramdisk[] and stops at the first entry with fid = -1, sets
- * it to the index of the entry in vramdisk[] (i.e. the actual file ID) and returns the same. If all
- * entries are exhausted, it means that no more available write space is left. Hence, ERR_VRAMDISK_FULL
- * is returned in that case. The file name too is set using the fname parameter passed. The file size
- * and file data are not touched.
+static int create_file(const char *fname, int *fid_addr) {
+/* Creates a new empty file with file name as fname.
+ * Linearly scans through all entries in vramdisk[] and stops at the first entry with fid = -1, sets
+ * it to the index of the entry in vramdisk[] (i.e. the actual file ID), stores the same at fid_addr,
+ * and returns 0 on success. If all entries are exhausted, it means that no more available write space
+ * is left. Hence, ERR_VRAMDISK_FULL is returned in that case. The file name too is set using the fname
+ * parameter passed. The file size and file data are not touched, assuming that they're already clear,
+ * i.e. either uninitialized or deleted.
  *
  * TODO: Optimize searching algorithm
  */
-  for (size_t i = 0; i < MAX_FILES; ++i) {
+  for (int i = 0; i < MAX_FILES; ++i) {
     if (vramdisk[i].fid == -1) {
       vramdisk[i].fid = i;
       strncpy(vramdisk[i].fname, fname, MAX_FNAME);
-      return vramdisk[i].fid;
+      *fid_addr = vramdisk[i].fid;
+      return 0;
     }
   }
   return ERR_VRAMDISK_FULL;
 }
 
-static ssize_t get_fsize_from_fid(ssize_t fid) {
-/* Get the file size from the file ID as in vramdisk[fid]. If the fid passed is negative, returns
- * ERR_INVALID_FILE_ID. If the fid value read from vramdisk[fid] is -1, it implies the file does
- * not exist for the given fid value, hence ERR_FILE_NOT_FOUND is returned.
+static int get_fsize_from_fid(int fid, size_t *fsize_addr) {
+/* Get the file size from the file ID as in vramdisk[fid], store it at fsize_addr, and return
+ * 0 on success. If the fid passed is out of bounds, returns ERR_INVALID_FILE_ID. If the fid
+ * value read from vramdisk[fid] is -1, it implies the file does not exist for the given fid
+ * value, hence ERR_FILE_NOT_FOUND is returned.
  */
   if (fid >= 0 && fid < MAX_FILES) {
-    if (vramdisk[fid].fid != -1)
-      return vramdisk[fid].fsize;
-    else
+    if (vramdisk[fid].fid != -1) {
+      *fsize_addr = vramdisk[fid].fsize;
+      return 0;
+    }
+    else {
       return ERR_FILE_NOT_FOUND;
+    }
+  }
+  return ERR_INVALID_FILE_ID;
+}
+
+static int truncate_file_from_fid(int fid) {
+ /* Truncate the contents of a file with the given file ID (fid). Essentially, clears the data
+  * buffer for the File struct with given fid and returns 0 on success. If the file ID passed
+  * is invalid, returns ERR_INVALID_FILE_ID.
+  */
+  if (fid >= 0 && fid < MAX_FILES) {
+    memset(vramdisk[fid].data, 0, vramdisk[fid].fsize);
+    vramdisk[fid].fsize = 0;
+    return 0;
   }
   return ERR_INVALID_FILE_ID;
 }
@@ -194,45 +217,79 @@ open (const char *pathname, int flags, ...) {
   strncpy(vramdisk[0].data, "Hello world!", MAX_FSIZE);
 
   ssize_t i = next_open_file_index;
-  ssize_t fid = -1;
+  int fid = -1;
+  int errcode;
 
   if (i >= MAX_FOPEN) {
-    errno = ENFILE;	// Max open files limit reached
+    errno = ENFILE;
     return -1;
   }
 
   switch (flags) {
 
     case MODE_R:
-      fid = find_file(pathname);
-      if (fid == ERR_FILE_NOT_FOUND) {
-        errno = ENOENT;		// File does not exist
+      errcode = find_file(pathname, &fid);
+      if (errcode == ERR_FILE_NOT_FOUND) {
+        errno = ENOENT;
         return -1;
       }
       open_files[i].fd = fid;
-      open_files[i].foffset = 0;
+      open_files[i].offset = 0;
       open_files[i].mode = MODE_R;
       break;
 
     case MODE_W:
-      fid = create_file(pathname);
-      if (fid == ERR_VRAMDISK_FULL) {
-        errno = ENOSPC;		// File system memory is full
-        return -1;
+      errcode = find_file(pathname, &fid);
+      if (errcode == ERR_FILE_NOT_FOUND) {
+        errcode = create_file(pathname, &fid);
+        if (errcode == ERR_VRAMDISK_FULL) {
+          errno = ENOSPC;
+          return -1;
+        }
+      }
+      else {
+        truncate_file_from_fid(fid);
+        /* We are not checking for the error code in
+         * truncate_file_from_fid() here because fid
+         * in this case is guaranteed to be valid.
+         * This function returns only ERR_INVALID_FILE_ID
+         * if fid is invalid, otherwise 0.
+         */
       }
       open_files[i].fd = fid;
-      open_files[i].foffset = 0;
+      open_files[i].offset = 0;
       open_files[i].mode = MODE_W;
       break;
     
     case MODE_A:
-      fid = create_file(pathname);
-      if (fid == ERR_VRAMDISK_FULL) {
-        errno = ENOSPC;		// File system memory is full
-        return -1;
+      errcode = find_file(pathname, &fid);
+      if (errcode == ERR_FILE_NOT_FOUND) {
+        errcode = create_file(pathname, &fid);
+        if (errcode == ERR_VRAMDISK_FULL) {
+          errno = ENOSPC;
+          return -1;
+        }
+      }
+      else {
+        truncate_file_from_fid(fid);
+        /* We are not checking for the error code in
+         * truncate_file_from_fid() here because fid
+         * in this case is guaranteed to be valid.
+         * This function returns only ERR_INVALID_FILE_ID
+         * if fid is invalid, otherwise 0.
+         */
       }
       open_files[i].fd = fid;
-      open_files[i].foffset = get_fsize_from_fid(fid);
+      size_t fsize;
+      get_fsize_from_fid(fid, &fsize);
+      /* get_fsize_from_fid() returns only two types of errors:
+       * ERR_FILE_NOT_FOUND and ERR_INVALID_FILE_ID. We are not
+       * checking for the error codes here because fid in this
+       * case is guaranteed to be valid, and only valid when the
+       * file exists. Otherwise, open() aborts after setting the
+       * appropriate errno.
+       */      
+      open_files[i].offset = fsize;
       open_files[i].mode = MODE_A;
       break;
   }
